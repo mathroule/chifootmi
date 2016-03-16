a1db162925cc3dd74cf53fc2b823250acc677065
mathroule <mathroule@gmail.com>
2016-03-16 22:11:21 +0100

    Added human and computer players + Tests

---
+++ src/main/java/com/mathroule/chifootmi/player/Computer.java
@@ -0,0 +1,32 @@
+package com.mathroule.chifootmi.player;
+
+import com.mathroule.chifootmi.element.Element;
+
+import java.util.Random;
+
+/**
+ * Implementation class for a computer player.
+ */
+public class Computer extends Player {
+
+    /**
+     * Computer available names.
+     */
+    private static final String[] AVAILABLE_NAMES = new String[]{
+            "Ana",
+            "Bob",
+            "Paul"
+    };
+
+    /**
+     * Random number generator used for name generation.
+     */
+    private static final Random random = new Random();
+
+    /**
+     * Computer player constructor using random name.
+     */
+    public Computer() {
+        super(AVAILABLE_NAMES[random.nextInt(AVAILABLE_NAMES.length - 1)]);
+    }
+}
---
+++ src/test/java/com/mathroule/chifootmi/player/HumanTest.java
@@ -0,0 +1,18 @@
+package com.mathroule.chifootmi.player;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test the human class.
+ */
+public class HumanTest {
+
+    @Test
+    public void testHuman() throws Exception {
+        Human human = new Human("Toto");
+
+        assertEquals("Toto", human.getName());
+    }
+}
