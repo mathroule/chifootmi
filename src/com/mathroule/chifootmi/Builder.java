5c9fe9bca439c9b85f7772fd8a70465eaa76a560
mathroule <mathroule@gmail.com>
2016-03-16 20:12:32 +0100

    Added builder interface

---
+++ src/com/mathroule/chifootmi/Builder.java
@@ -0,0 +1,14 @@
+package com.mathroule.chifootmi;
+
+/**
+ * Builder interface.
+ *
+ * @author Mathieu Rul <mathroule@gmail.com>
+ * @project Chifootmi
+ * @created 16/03/2016
+ * @since 1.0
+ */
+public interface Builder<T> {
+
+    T build();
+}
