7d839b922bf2323fa30e31cd9728ee5d529968f0
Mathieu Rul <marul-ext@airfrance.fr>
2016-03-17 14:06:44 +0100

    Added missing weapons tests

--- src/test/java/com/mathroule/chifootmi/weapon/LizardTest.java
+++ src/test/java/com/mathroule/chifootmi/weapon/LizardTest.java
@@ -19,17 +19,56 @@ public class LizardTest {
 
     @Test
     public void testEqualsWithRock() throws Exception {
-        // TODO
+        Lizard spock1 = new Lizard();
+        Rock rock1 = new Rock();
+        assertFalse(spock1.equals(rock1));
+        assertFalse(rock1.equals(spock1));
+
+        Weapon spock2 = new Lizard();
+        assertFalse(spock2.equals(rock1));
+        assertFalse(rock1.equals(spock2));
+
+        Weapon rock2 = new Rock();
+        assertFalse(rock2.equals(spock1));
+        assertFalse(spock1.equals(rock2));
+        assertFalse(rock2.equals(spock2));
+        assertFalse(spock2.equals(rock2));
     }
 
     @Test
     public void testEqualsWithPaper() throws Exception {
-        // TODO
+        Lizard lizard1 = new Lizard();
+        Paper paper1 = new Paper();
+        assertFalse(paper1.equals(lizard1));
+        assertFalse(lizard1.equals(paper1));
+
+        Weapon lizard2 = new Lizard();
+        assertFalse(lizard2.equals(paper1));
+        assertFalse(paper1.equals(lizard2));
+
+        Weapon paper2 = new Paper();
+        assertFalse(paper2.equals(lizard1));
+        assertFalse(lizard1.equals(paper2));
+        assertFalse(paper2.equals(lizard2));
+        assertFalse(lizard2.equals(paper2));
     }
 
     @Test
     public void testEqualsWithScissors() throws Exception {
-        // TODO
+        Lizard lizard1 = new Lizard();
+        Scissors scissors1 = new Scissors();
+        assertFalse(lizard1.equals(scissors1));
+        assertFalse(scissors1.equals(lizard1));
+
+        Weapon lizard2 = new Lizard();
+        assertFalse(lizard2.equals(scissors1));
+        assertFalse(scissors1.equals(lizard2));
+
+        Weapon scissors2 = new Scissors();
+        assertFalse(scissors2.equals(lizard1));
+        assertFalse(lizard1.equals(scissors2));
+        assertFalse(scissors2.equals(lizard2));
+        assertFalse(lizard2.equals(scissors2));
     }
 
     @Test
@@ -51,6 +90,19 @@ public class LizardTest {
 
     @Test
     public void testEqualsWithSpock() throws Exception {
-        // TODO
+        Lizard lizard1 = new Lizard();
+        Spock spock1 = new Spock();
+        assertFalse(lizard1.equals(spock1));
+        assertFalse(spock1.equals(lizard1));
+
+        Weapon lizard2 = new Lizard();
+        assertFalse(lizard2.equals(spock1));
+        assertFalse(spock1.equals(lizard2));
+
+        Weapon spock2 = new Spock();
+        assertFalse(spock2.equals(lizard1));
+        assertFalse(lizard1.equals(spock2));
+        assertFalse(spock2.equals(lizard2));
+        assertFalse(lizard2.equals(spock2));
     }
 }
