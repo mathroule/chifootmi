5521f30e9388d23e0729c8d3b9873958e170d51a
Mathieu Rul <marul-ext@airfrance.fr>
2016-03-17 18:13:37 +0100

    Added computer vs computer mode

--- src/main/java/com/mathroule/chifootmi/Main.java
+++ src/main/java/com/mathroule/chifootmi/Main.java
@@ -1,5 +1,10 @@
 package com.mathroule.chifootmi;
 
+import com.mathroule.chifootmi.game.Game;
+import com.mathroule.chifootmi.game.Match;
+import com.mathroule.chifootmi.game.rule.Basic;
+import com.mathroule.chifootmi.game.rule.Extended;
+
 import java.util.Arrays;
 
 /**
@@ -9,5 +14,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("args: " + Arrays.asList(args));
+
+
+        Game.getInstance().play(Match.Mode.COMPUTER_VS_COMPUTER, 3, new Basic());
+        Game.getInstance().play(Match.Mode.COMPUTER_VS_COMPUTER, 5, new Extended());
     }
 }
--- src/main/java/com/mathroule/chifootmi/game/rule/Basic.java
+++ src/main/java/com/mathroule/chifootmi/game/rule/Basic.java
@@ -4,26 +4,39 @@ package com.mathroule.chifootmi.game.rule;
 import com.mathroule.chifootmi.game.weapon.Paper;
 import com.mathroule.chifootmi.game.weapon.Rock;
 import com.mathroule.chifootmi.game.weapon.Scissors;
+import com.mathroule.chifootmi.game.weapon.Weapon;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Implement the basic games rules (i.e. Rock-paper-scissors).
  */
 public class Basic extends Rules {
 
-
     // TODO @see: https://en.wikipedia.org/wiki/Interface_segregation_principle
+    // TODO @see: https://en.wikipedia.org/wiki/Dependency_inversion_principle
 
     /**
      * Constructor of the basic game rules (i.e. Rock-paper-scissors).
      */
     public Basic() {
-        // Scissors cuts paper
-        this.add(new Rule(new Scissors(), "cuts", new Paper()));
+        // Rock crushes scissors
+        this.add(new Rule(new Rock(), "crushes", new Scissors()));
 
         // Paper covers rock
         this.add(new Rule(new Paper(), "covers", new Rock()));
 
-        // Rock crushes scissors
-        this.add(new Rule(new Rock(), "crushes", new Scissors()));
+        // Scissors cuts paper
+        this.add(new Rule(new Scissors(), "cuts", new Paper()));
+    }
+
+    @Override
+    public List<Weapon> getAvailableWeapons() {
+        List<Weapon> weapons = new ArrayList<>();
+        weapons.add(new Rock());
+        weapons.add(new Paper());
+        weapons.add(new Scissors());
+        return weapons;
     }
 }
